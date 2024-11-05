f46e3987a90b89da52da3a6d9ff55d444df0e356
selfxliw <selfxliw@gmail.com>
2024-11-05 18:58:00 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,14 @@
+text = "hello world"
+name = "Ліза"
+surname = "Карпюк"
+age = 16
+
+print("Тип змінної 'text':", type(text))
+print("Тип змінної 'name':", type(name))
+print("Тип змінної 'surname':", type(surname))
+print("Тип змінної 'age':", type(age))
+
+print("Значення 'text':", text)
+print("Значення 'name':", name)
+print("Значення 'surname':", surname)
+print("Значення 'age':", age)
