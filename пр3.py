7a6b6bc86aa91fe1d897f599d94b02857b942c52
selfxliw <selfxliw@gmail.com>
2024-11-28 12:43:31 +0200

    Update пр3.py

--- пр3.py
+++ пр3.py
@@ -1,14 +1,23 @@
-def remove_duplicates(lst):
-    return list(set(lst))
+main_dict = {
+    "key1": "50",
+    "key2": "Hello",
+    "key3": {
+        "subkey1": "3.14",
+        "subkey2": 100,
+        "subkey3": False,
+        "subkey4": [1, 2, 3],
+        "subkey5": None
+    },
+    "key4": True
+}
 
-def sort_list(lst):
-    numbers = sorted([item for item in lst if isinstance(item, (int, float))])
-    strings = sorted([item for item in lst if isinstance(item, str)])
-    return numbers + strings
+types_dict = {
+    key: type(value) for key, value in main_dict.items()
+}
 
-initial_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
-unique_list = remove_duplicates(initial_list)
-sorted_list = sort_list(unique_list)
+nested_types_dict = {
+    subkey: type(subvalue) for subkey, subvalue in main_dict["key3"].items()
+}
 
-print("Список без повторень:", unique_list)
-print("Відсортований список:", sorted_list)
+print("Словник з типами даних основного словника:", types_dict)
+print("Словник з типами даних вкладеного словника:", nested_types_dict)
