06ce6a89b0e4e1c47fdd702e98c08157c6169db7
selfxliw <selfxliw@gmail.com>
2024-12-07 19:42:16 +0200

    Add files via upload

--- weather_project/main.py
+++ weather_project/main.py
@@ -1,1 +1,25 @@
+from fetcher import fetch_weather_data
+from parser import parse_weather_data
 
+def get_weather(url: str):
+    """
+    Отримує та розбирає дані про погоду за заданою URL-адресою.
+
+    :param url: URL-адреса веб-сайту з прогнозом погоди
+    :return: Список температур на сьогодні
+    """
+    html = fetch_weather_data(url)
+    temperatures = parse_weather_data(html)
+    print("Температура на сьогодні:")
+    for temp in temperatures:
+        print(f" - {temp}")
+
+def main():
+    """
+    Головна функція для виконання скрипта отримання погоди.
+    """
+    url = 'https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BB%D1%83%D1%86%D1%8C%D0%BA'
+    get_weather(url)
+
+if __name__ == '__main__':
+    main()
--- weather_project/parser.py
+++ weather_project/parser.py
@@ -1,1 +1,12 @@
+from bs4 import BeautifulSoup
 
+def parse_weather_data(html: str):
+    """
+    Розбирає дані про погоду з HTML-контенту.
+
+    :param html: HTML-контент у вигляді рядка
+    :return: Список температур на сьогодні
+    """
+    soup = BeautifulSoup(html, 'html.parser')
+    temperatures = soup.find_all('p', class_='today-temp')
+    return [temp.text for temp in temperatures]
